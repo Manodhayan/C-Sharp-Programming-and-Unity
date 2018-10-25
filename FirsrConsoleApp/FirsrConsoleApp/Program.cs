06a44e7caa6439aa555603dc330b216b873d92a7
manodhayan <manodhayan@hotmail.com>
2018-10-26 00:47:37 +0530

    First commit

---
+++ FirsrConsoleApp/FirsrConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirsrConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm Manodhayan!!! doing C# Programming");
+            Console.WriteLine();
+        }
+    }
+}
